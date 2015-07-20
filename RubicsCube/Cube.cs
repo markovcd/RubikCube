2098f5107276111d59c4a8e1b3506d3e8e2ad808
markovcd <markovcd@gmail.com>
2015-07-20 15:52:27 +0200

    Finished initial implementation

--- RubicsCube/Cube.cs
+++ RubicsCube/Cube.cs
@@ -1,112 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace RubiksCube
-{
-    public enum Axis { X, Y, Z }
-    public enum Direction { Left, Right }
-    
-    public class Point
-    {
-        public int Value;
-
-        public bool this[Axis a]
+﻿using System; using System.Collections.Generic; using System.Linq;  namespace RubiksCube {     public enum Axis { X, Y, Z }          public class Point     {         public int Value;          public bool this[Axis a]         {             get { return Convert.ToBoolean(Value / (int)Math.Pow(2, (int)a) % 2); }             set             {                 var m = (int)Math.Pow(2, (int)a);                 Value -= (Value / m) % 2 * m;                 Value += Convert.ToInt32(value) * m;             }         }          public Point(bool x, bool y, bool z)          {             this[Axis.X] = x;             this[Axis.Y] = y;             this[Axis.Z] = z;         }          public Point(int value)         {             Value = value;         }                  public void Transform(Axis a1, Axis a2)         {             var tmp = this[a1];             this[a1] = !this[a2];             this[a2] = tmp;         }          public override string ToString()         {             return String.Format("{0} {1} {2}", this[Axis.X]?1:0, this[Axis.Y]?1:0, this[Axis.Z]?1:0);         }     }      public class Cubelet     {         public Cubelet(Point p, int xy, int yz, int xz)         {             this[Axis.X, Axis.Y] = xy;             this[Axis.Y, Axis.Z] = yz;             this[Axis.X, Axis.Z] = xz;             Location = p;         }          public int Value;          public Point Location { get; set; }          public int this[Axis a1, Axis a2]         {             get             {                 int a = (int)a1 + (int)a2 - 1;                 return Value / (int)Math.Pow(10, a) % 10;             }             set             {                 int a = (int)a1 + (int)a2 - 1;                 var m = (int)Math.Pow(10, a);                 Value -= (Value / m) % 10 * m;                 Value += value * m;             }         }          public void Transform(Axis a1, Axis a2)         {
+            var a = (Axis)(3 - (int)a1 - (int)a2);              var tmp = this[a, a1];             this[a, a1] = this[a, a2];             this[a, a2] = tmp;                          Location.Transform(a1, a2);         } 
+        public override string ToString()
         {
-            get { return Convert.ToBoolean(Value / (int)Math.Pow(2, (int)a) % 2); }
-            set
+            return String.Format("L {0} XY {1} XZ {2} YZ {3}", Location, 
+                this[Axis.X, Axis.Y], this[Axis.X, Axis.Z], this[Axis.Y, Axis.Z]);
+        }
+    }      public class Cube     {         public IList<Cubelet> Cubelets { get; private set; }          public Cube()         {             Cubelets = new Cubelet[8];              for (int i = 0; i < 8; i++)             {                 var p = new Point(i);                 Cubelets[i] = new Cubelet(p, p[Axis.Z]?2:1, p[Axis.X]?4:3, p[Axis.Y]?6:5);             }         }                  public IEnumerable<Cubelet> GetSide(Axis a1, Axis a2, bool side)         {
+            var a = (Axis)(3 - (int)a1 - (int)a2);             return Cubelets.Where(c => c.Location[a] == side);                }                  public int[,] GetFace(Axis a1, Axis a2, bool side)         {             var result = new int[2,2];                          foreach (var c in GetSide(a1, a2, side))             {                 int i = c.Location[a1] ? 1 : 0;                 int j = c.Location[a2] ? 1 : 0;                 result[i, j] = c[a1, a2];             }                                              return result;         }
+
+        public bool IsFinished(Axis a1, Axis a2, bool side)
+        {
+            int? j = null;
+            foreach (var i in GetFace(a1, a2, side))
             {
-                var m = (int)Math.Pow(2, (int)a);
-                Value -= (Value / m) % 2 * m;
-                Value += Convert.ToInt32(value) * m;
+                if (j == null) j = i;
+                if (j != i) return false;              
             }
+            return true;
         }
 
-        public Point(bool x, bool y, bool z) 
+        public bool IsFinished()
         {
-            this[Axis.X] = x;
-            this[Axis.Y] = y;
-            this[Axis.Z] = z;
-        }
+            bool b = true;
 
-        public Point(int value)
-        {
-            Value = value;
-        }
-    }
+            for (int a1 = 0; a1 < 2; a1++)
+                for (int a2 = a1+1; a2 < 3; a2++)
+                {
+                    b = b && IsFinished((Axis)a1, (Axis)a2, false);
+                    b = b && IsFinished((Axis)a1, (Axis)a2, true);
+                }
 
-    public class Cubelet
-    {
-        public Cubelet(Point p, int xy, int yz, int xz)
-        {
-            this[Axis.X, Axis.Y] = xy;
-            this[Axis.Y, Axis.Z] = yz;
-            this[Axis.X, Axis.Z] = xz;
-            Location = p;
-        }
+            return b;
+        }          public void Transform(Axis a1, Axis a2, bool side)         {             GetSide(a1, a2, side).ToList().ForEach(c => c.Transform(a1, a2));         }
 
-        public int Value;
-
-        public Point Location { get; set; }
-
-        public int this[Axis a1, Axis a2]
-        {
-            get
-            {
-                int a = (int)a1 + (int)a2 - 1;
-                return Value / (int)Math.Pow(10, a) % 10;
-            }
-            set
-            {
-                int a = (int)a1 + (int)a2 - 1;
-                var m = (int)Math.Pow(10, a);
-                Value -= (Value / m) % 10 * m;
-                Value += value * m;
-            }
-        }
-
-        public void Transform(Axis a1, Axis a2)
-        {
-            var pivot = GetPivot(a1, a2);
-
-            var tmp = this[pivot, a1];
-            this[pivot, a1] = this[pivot, a2];
-            this[pivot, a2] = tmp;
-        }
-
-        public static Axis GetPivot(Axis a1, Axis a2)
-        {
-            int i = 3 - (int)a1 - (int)a2;
-            return (Axis)i;
-        }
-
-    }
-
-    public class Cube
-    {
-        public IList<Cubelet> Cubelets { get; private set; }
-
-        public Cube()
-        {
-            Cubelets = new Cubelet[8];
-
-            for (int i = 0; i < 8; i++)
-            {
-                var p = new Point(i);
-                Cubelets[i] = new Cubelet(p, p[Axis.Z]?2:1, p[Axis.X]?4:3, p[Axis.Y]?6:5);
-            }
-        }
-
-        public void RotateFace(Axis a1, Axis a2, bool side)
-        {
-            var pivot = Cubelet.GetPivot(a1, a2);
-            var cubelets = Cubelets.Where(c => c.Location[pivot] == side).ToList();
-
-            foreach (var c in cubelets)
-            {
-                c.Transform(a1, a2); 
-                c.Location[]
-            }
-        }
-    }
-}
+        public void Transform(Axis a1, Axis a2)         {
+            Transform(a1, a2, false);
+            Transform(a1, a2, true);         }     } } 
--- RubicsCube/Program.cs
+++ RubicsCube/Program.cs
@@ -5,50 +5,19 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RubicsCube
+namespace RubiksCube
 {
     class Program
     {
-        static void Print(Face f)
-        {
-            Console.WriteLine("{0} {1}", f[new Point(false, false)], f[new Point(false, true)]);
-            Console.WriteLine("{0} {1}", f[new Point(true, false)], f[new Point(true, true)]);
-            Console.WriteLine();
-        }
-        
+       
         static void Main(string[] args)
         {
-            var f = new Face();
-            f.Value = 4321;
-
-
-            f[Direction.Up] = new Face{Value = 4444};
-            f[Direction.Down] = new Face{Value = 3333};
-            f[Direction.Left] = new Face { Value = 2222 };
-            f[Direction.Right] = new Face { Value = 1111 };
-
-       
-            Print(f[Direction.Up]);
-            Print(f[Direction.Down]);
-            Print(f[Direction.Left]);
-            Print(f[Direction.Right]);
-
-            Print(f);
-
-            f.Move(Direction.Left);
-
-
-
-            Print(f[Direction.Up]);
-            Print(f[Direction.Down]);
-            Print(f[Direction.Left]);
-            Print(f[Direction.Right]);
-
-            Print(f);
-
-
-
-            Console.ReadLine();
+            var cube = new Cube();
+            var a = cube.GetFace(Axis.X, Axis.Y, false);
+            cube.RotateFace(Axis.X, Axis.Z, false);
+            var f = cube.GetFace(Axis.X, Axis.Y, false);
+            cube.RotateFace(Axis.X, Axis.Y, false);
+            var g = cube.GetFace(Axis.X, Axis.Y, false);
         }
     }
 }
